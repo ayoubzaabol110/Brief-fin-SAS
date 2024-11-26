89145f476050a63862eff9a6d9b1627a42107af2
ayoubzaabol110 <ayoubfatihi916@gmail.com>
2024-11-26 10:31:02 +0100

    afficher le menu

---
+++ BREIF FIN SAS (1).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+  do {
+        printf("1 - Ajouter une tâche :\n");
+        printf("2 - Afficher les tâches :\n");
+        printf("3 - Supprimer une tâche :\n");
+        printf("4 - Modifier une tâche :\n");
+        printf("5 - Filtrer les tâches par priorité\n");
+        printf("0 - Quitter\n");
+
+        
+    } while (choix != 0);
+
+    return 0;
+}
